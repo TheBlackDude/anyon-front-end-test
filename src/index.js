9ffa4586420d8fa42916928985e3c992dc3c6a03
Osman Jalloh <mudu.jalloh@gn.ehealthafrica.org>
2018-01-13 14:24:23 +0100

    feat: setup base configuration.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,50 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import './index.css';
-import App from './App';
+
+import { combineReducers, createStore, applyMiddleware } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import { Provider } from 'react-redux'
+import { createHashHistory } from 'history'
+import { connectRouter, routerMiddleware, ConnectedRouter } from 'connected-react-router'
+
+import App from './home/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+/*
+ * the hashHistory
+ */
+const history = createHashHistory()
+
+/*
+ * the main store
+ */
+const createStoreWithState = (initialState = {}) => createStore(
+  connectRouter(history)(
+    combineReducers({
+      // TODO reducers
+    })
+  ),
+  initialState,
+  composeWithDevTools(
+    applyMiddleware(
+      routerMiddleware(history)
+      // add other middlewares here
+    )
+    // add other enhancers here
+  )
+)
+
+/*
+ * connect the app with the store
+ */
+const store = createStoreWithState()
+ReactDOM.render((
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <App />
+    </ConnectedRouter>
+  </Provider>
+), document.getElementById('root'))
 registerServiceWorker();
